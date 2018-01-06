8e266f7dc54e297de125796bd9f3db716ff29e76
Denghejun <hjdeng@CNhjdeng.local>
2018-01-06 18:02:10 +0800

    add drawer

--- src/types/Actions.ts
+++ src/types/Actions.ts
@@ -1,5 +1,5 @@
 export const TYPE_ACTION = {
-    APP: Symbol('APP_ACTION'),
-    HOME: Symbol('HOME_ACTION'),
-    MOVIE: Symbol('MOVIE_ACTION')
+  APP: Symbol('APP_ACTION'),
+  HOME: Symbol('HOME_ACTION'),
+  MOVIE: Symbol('MOVIE_ACTION')
 }
--- src/types/ReducerCombiners.ts
+++ src/types/ReducerCombiners.ts
@@ -1,5 +1,7 @@
 export const TYPE_REDUCER_COMBINER = {
   APP: Symbol('APP'),
   HOME: Symbol('HOME'),
-  MOVIE: Symbol('MOVIE')
+  MOVIE: Symbol('MOVIE'),
+  USER_DETAIL: Symbol('USER_DETAIL'),
+  DRAWER_CONTENT: Symbol('DRAWER_CONTENT')
 }
